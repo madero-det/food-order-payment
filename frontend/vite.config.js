7bb7ad0126ec30ff785c294c4f2bc8546608a5c1
Madero <madero@mcnc.co.kr>
2026-07-24 23:43:35 +0700

    Suppress chunk size warning in Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
