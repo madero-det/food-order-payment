322ef804bbf39abd5d75446614fda20d4cb3eaf1
Madero <madero@mcnc.co.kr>
2026-07-26 17:12:40 +0700

    Exclude is_rice items from food dropdown (rice has its own checkbox)

--- frontend/src/components/OrderForm.jsx
+++ frontend/src/components/OrderForm.jsx
@@ -91,8 +91,8 @@ export default function OrderForm({ persons, menuItems = [], onSubmit, initialDa
     onSubmit(data);
   };
 
-  const foodItems = menuItems.filter(m => m.type !== 'dessert');
-  const dessertItems = menuItems.filter(m => m.type === 'dessert');
+  const foodItems = menuItems.filter(m => m.type !== 'dessert' && !m.is_rice);
+  const dessertItems = menuItems.filter(m => m.type === 'dessert' && !m.is_rice);
     const riceItem = menuItems.find(m => m.is_rice);
   const hasRice = selectedItems.some(si => riceItem && si.menu_item_id === riceItem.id);
 
